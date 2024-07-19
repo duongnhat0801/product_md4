3e82828c9f0ae32a44c63851562db105417aa957
duongnhat0801 <nhatx0801@hmail.com>
2024-07-19 09:27:14 +0700

    update persist

--- src/main/java/com/codegym/product_baitap/repositories/ProductRepo.java
+++ src/main/java/com/codegym/product_baitap/repositories/ProductRepo.java
@@ -8,31 +8,32 @@ import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
-import java.util.ArrayList;
 import java.util.List;
 
 @Transactional
 @Repository
 public class ProductRepo implements IProductRepo {
-    private static final List<Product> products = new ArrayList<>();
 
     @PersistenceContext
     private EntityManager entityManager;
 
     @Override
     public List<Product> showAll() {
-        List<Product> products = entityManager.createQuery("select p from Product p", Product.class).getResultList();
-        return products;
+        return entityManager.createQuery("select p from Product p", Product.class).getResultList();
     }
 
     @Override
     public void save(Product product) {
-        entityManager.merge(product);
+        if (product.getId() == null) {
+            entityManager.persist(product);
+        } else {
+            entityManager.merge(product);
+        }
     }
 
     @Override
     public Product findById(Long id) {
-        TypedQuery<Product> query = entityManager.createQuery("select p from Product p where  p.id=:id", Product.class);
+        TypedQuery<Product> query = entityManager.createQuery("select p from Product p where p.id = :id", Product.class);
         query.setParameter("id", id);
         try {
             return query.getSingleResult();
@@ -51,8 +52,8 @@ public class ProductRepo implements IProductRepo {
 
     @Override
     public List<Product> searchProductByName(String keyword) {
-        List<Product> products = entityManager.createQuery("select p from Product p where name like :name", Product.class).setParameter("name","%" + keyword + "%").getResultList();
-        return products;
+        return entityManager.createQuery("select p from Product p where p.name like :name", Product.class)
+                .setParameter("name", "%" + keyword + "%")
+                .getResultList();
     }
-
 }
